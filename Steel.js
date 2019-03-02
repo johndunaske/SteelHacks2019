fc4d202818b7937dc4841d8f040aef8d2665533b
ImaniASC4 <imani-10@live.com>
2019-03-02 00:47:23 -0500

    Chicken

--- Steel.js
+++ Steel.js
@@ -1,17 +1,2 @@
-var key='21993a36882f4eb48b98a4a27e20f4ec';
 
-function subfinder(that){
-    var url ="https://newsapi.org/v2/everything?q="+that+"&apiKey="+key;
-    var request = new Request(url);
-    var list=[""]
-    var articlefetch = {}
-    fetch(request)
-        .then(function(response){
-            articlefetch=json.parse(response.json());
-        })
-        articlefetch=articlefetch["articles"];
-    for(var article=0;article<articlefetch.length;article++){
-        list+=articlefetch[article]["content"]
-    }
-    return list
-}
+
