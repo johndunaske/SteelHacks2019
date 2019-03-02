af65cbe8635d632277a864a546a2ca07912f69ac
Logan Ramhap <lramhap@gmail.com>
2019-03-02 11:39:12 -0500

    Update charts.js

--- templates/charts.js
+++ templates/charts.js
@@ -1,3 +1,23 @@
+import main.js
+var Publishers = main.articles
+var Art_Titles = main.contents
+var scores = main.scores
+var tpositive = 0
+var tnegative = 0
+
+for (int z = 0; x < scores.length; x++){
+  if (scores[z] >= 0.0){
+    barData[0]["y"].push(score[z])
+    barData[0]["x"].push("Publisher: " + Publishers[z] + '\n' + "Article: " + Art_Titles[z])
+    tpositive += 1
+  }else{
+    barData[1]["base"].push(score[z])
+    barData[1]["y"].push(Math.abs(score[z]))
+    barData[1]["x"].push("Publisher: " + Publishers[z] + '\n' + "Article: " + Art_Titles[z])
+    tnegative += 1
+  }
+}
+
 var barData = [
   {
     type: 'bar',
@@ -23,7 +43,7 @@ var barData = [
 Plotly.newPlot('tConnotation', data);
 
 var piData = [{
-  values: [19, 26, 55],
+  values: [tpositive, tnegative],
   labels: ['Positive Connotation', 'Negative Connotation'],
   type: 'pie'
 }];
