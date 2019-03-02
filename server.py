c124e23fa3dd007e29f4948db56b959d1fc00c1e
John K Dunaske <j_dunaske@yahoo.com>
2019-03-02 14:58:33 -0500

    grindr

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def html():
-    return render_template('/searchpage.html')
+    return render_template('/index.html')
 
 
 @app.route('/main.js')
@@ -22,9 +22,57 @@ def js():
 def charts():
     return render_template('charts.js')
 
-@app.route('/indexcss.css')
+@app.route('/resume.js')
+def res():
+    return render_template('js/resume.js')
+
+@app.route('/resume.min.js')
+def resm():
+    return render_template('js/resume.min.js')
+
+@app.route('/js/resume.min.js')
+def jsresm():
+    return render_template('js/resume.min.js')
+
+@app.route('/jquery.js')
+def jq():
+    return render_template('vendor/jquery/resume.js')
+
+@app.route('/vendor/bootstrap/js/bootstrap.bundle.min.js')
+def jqm():
+    return render_template('vendor/bootstrap/js/bootstrap.bundle.min.js')
+
+@app.route('/vendor/jquery/jquery.min.js')
+def jqmv():
+    return render_template('vendor/jquery/jquery.min.js')
+
+@app.route('/vendor/jquery-easing/jquery.easing.min.js')
+def jqe():
+    return render_template('vendor/jquery-easing/jquery.easing.min.js')
+#static/webfonts/fa-brands-400.woff2
+@app.route('/static/webfonts/fa-brands-400.woff2')
+def fontwoff2():
+    return render_template('vendor/fontawesome-free/webfonts/fa-brands-400.woff2')
+
+@app.route('/static/webfonts/fa-brands-400.woff')
+def fontwoff():
+    return render_template('vendor/fontawesome-free/webfonts/fa-brands-400.woff')
+
+@app.route('/static/webfonts/fa-brands-400.ttf')
+def fontttf():
+    return render_template('vendor/fontawesome-free/webfonts/fa-brands-400.ttf')
+
+@app.route('/static/webfonts/fa-brands-400.woff2')
+def fontttf9():
+    return render_template('vendor/fontawesome-free/webfonts/fa-solid-900.woff')
+
+@app.route('/bootstrap.css')
 def css():
-    return render_template('indexcss.css')
+    return render_template('vendor/bootstrap/css/bootstrap.css')
+
+@app.route('/bootstrap.min.css')
+def cssM():
+    return render_template('vendor/bootstrap/css/bootstrap.min.css')
 
 
 @app.route('/data')
