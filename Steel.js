a9237ac0966bd75768ee9d9ca63455c9fc2e2bd1
ImaniASC4 <imani-10@live.com>
2019-03-02 00:31:52 -0500

    Update Steel.js

--- Steel.js
+++ Steel.js
@@ -1,43 +1,17 @@
 var key='21993a36882f4eb48b98a4a27e20f4ec';
 
-function byCountryfetcher(country){
-    var countryfetch={}
-    var url = 'https://newsapi.org/v2/top-headlines?' +
-              'country='+country+'&' +
-              'apiKey='+key;
-    var request = new Request(url);
-    fetch(request)
-        .then(function(response) {
-            countryfetch =json.parse(response.json());
-        })
-    for(var article=0;article<countryfetch["articles"];article++){
-
-    }
-}
-function articlesabout(that){
+function subfinder(that){
     var url ="https://newsapi.org/v2/everything?q="+that+"&apiKey="+key;
     var request = new Request(url);
     var list=[""]
+    var articlefetch = {}
     fetch(request)
         .then(function(response){
             articlefetch=json.parse(response.json());
         })
-    for(var article=0;article<articlefetch["articles"].length;article++){
-        list+=article["content"]
+        articlefetch=articlefetch["articles"];
+    for(var article=0;article<articlefetch.length;article++){
+        list+=articlefetch[article]["content"]
     }
     return list
-}
-function pubFinder(source){
-    var url="https://newsapi.org/v2/top-headlines?sources="+souurce+"&apiKey=21993a36882f4eb48b98a4a27e20f4ec"
-    var sourcerequest={}
-    var motherlist=[""]
-    var request = new Request(url);
-    fetch(request)
-        .then(function(response){
-            sourcerequest=json.parse(response.json());
-        })
-    for(var article=0;article<sourcerequest["articles"];article++){
-        motherlist+=article["content"]
-    }
-    return motherlist
 }
