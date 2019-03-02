9b2afcc3013dca48951b62bb680dabfc1996923b
Logan Ramhap <lramhap@gmail.com>
2019-03-01 23:13:53 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 import plotly
 var source = ""
-function pub{
+function pub(){
   source = "Publication"
 }
-function sub{
+function sub(){
   source = "Subject"
 }
-function 
+function url(){
+  source = "Url"
+}
+
+function fchooser(source){
+  if (source == "Publication"){
+    pubFinder("put the input from textbox on second page here")
+  }else if (source == "Subject") {
+    subFinder("put the input from textbox on second page here")
+  }else if(source == "Url"){
+    urlFinder("put the input from textbox on second page here")
+  }else{
+    "2nd page textbox.text" = "Source Not Found"
+  }
+}
+function pubFinder(publication){
+
+}
+function subFinder(subject){
+
+}
+function urlFinder(url){
+
+}
